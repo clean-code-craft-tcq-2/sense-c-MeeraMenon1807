0fa332015f90592e5bb8972f4b46a8f59afb6e35
MeeraMenon1807 <91048876+MeeraMenon1807@users.noreply.github.com>
2021-10-25 12:12:56 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,7 +1,5 @@
 #include "stats.h"
 
-int emailAlertCallCount = 0;
-int ledAlertCallCount = 0;
 Stats compute_statistics(const float* numberset, int setlength) {
 	Stats s;
 	s.average = 0;
@@ -45,22 +43,3 @@ Stats s;
 return s;	
 }
 
-void emailAlerter()
-{	
-   emailAlertCallCount = 1;
-}
-void ledAlerter()
-{
-   ledAlertCallCount = 1;
-}
-
-void check_and_alert(float maxThreshold,alerter_funcptr alerters[], Stats computedStats)
-{
-	if(computedStats.max > maxThreshold)
-	{
-		alerters[0]();
-		alerters[1]();
-			
-	}
-	
-}
